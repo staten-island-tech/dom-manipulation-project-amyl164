4543156f144f444e3994d4a3f6c2316e1e078b5b
amyl164 <amyl81008@gmail.com>
2023-10-17 22:12:33 -0400

    gegawaesdf

--- dom.js
+++ dom.js
@@ -1,28 +1,51 @@
+/*
 const DOMSelectors ={
     form: document.querySelector("#form"),    
-    firstname:document.querySelector("#firstName"),
-    lastname: document.querySelector(".last-name"),
+    firstname:document.querySelector(".firstname"),
+    lastname: document.querySelector(".lastname"),
     osis: document.querySelector(".os-is"),
-
+    firstnameoutput: document.querySelector("#firstnameoutput"),
+    lastnameoutput: document.querySelector("#lastnameoutput"),
+    osisoutput: document.querySelector("#osisoutput"),
+};
     //select the textbox 
     //select all the h2s in one property
-};
-DOMSelectors.form.addEventListener("submit", function(event){
-    event.preventDefault();
-    console.log(DOMSelectors.firstName.value); 
-    DOMSelectors.firstNameoutput.innerText = firstName
-});
 
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log(DOMSelectors.lastname.value); 
-   
-       
+    console.log(DOMSelectors.firstname.value);
+});
+DOMSelectors.firstnameoutput.innerText = firstname
+
+
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.lastname.value);       
 });
 
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
     console.log(DOMSelectors.osis.value); 
-  
-       
+        
+});
+*/
+
+const DOMSelectors ={
+    form: document.querySelector("#form"),    
+    firstname:document.querySelector(".firstname"),
+    lastname: document.querySelector(".lastname"),
+    osis: document.querySelector(".osis"),
+    firstnameoutput: document.querySelector("#firstnameoutput"),
+    lastnameoutput: document.querySelector("#lastnameoutput"),
+    osisoutput: document.querySelector("#osisoutput"),
+};
+
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    const firstname = DOMSelectors.firstname.value
+    const lastname = DOMSelectors.lastname.value
+    const osis = DOMSelectors.osis.value
+    DOMSelectors.firstnameoutput.innerText = firstname
+    DOMSelectors.lastnameoutput.innerText = lastname
+    DOMSelectors.osisoutput.innerText = osis
 });
