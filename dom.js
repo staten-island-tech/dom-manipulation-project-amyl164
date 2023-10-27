c7e9012a723d9751e3a2586b02309dd172605d2d
amyl164 <amyl81008@gmail.com>
2023-10-27 10:14:00 -0400

    S

--- dom.js
+++ dom.js
@@ -3,40 +3,76 @@ const DOMSelectors ={
     dogbreed:document.querySelector(".dogbreed"),
     name: document.querySelector(".name"),
     pic: document.querySelector(".pic"),
-    dogbreedoutput: document.querySelector("#dogbreedoutput"),
-    nameoutput: document.querySelector("#nameoutput"),
-    picoutput: document.querySelector("#picoutput"),
     container: document.querySelector(".container"),
     card: document.querySelector(".card"),
     cardimg: document.querySelector(".cardimg"),
-    button: document.querySelector(".btn"),
   
 };
+let count = 0;
 function insert(){
 DOMSelectors.form.addEventListener("submit", function(event){
+    count++
     event.preventDefault();
     const dogbreed = DOMSelectors.dogbreed.value
     const name = DOMSelectors.name.value
     const pic = DOMSelectors.pic.value
     function divCreator(){
-    DOMSelectors.container.insertAdjacentHTML(
-        "afterbegin",
-        `<div class="container"> <div class="card"><h3>${name}</h3> <img src="${pic}" alt=""class="cardimg"><h4>${dogbreed}</h4> <button class="btn">Remove</button></div></div>`
+        DOMSelectors.container.insertAdjacentHTML(
+            "afterbegin",
+            `<div id=${count} class="container"> <div class="card"><h3>${name}</h3> <img src="${pic}" alt=""class="cardimg"><h4>${dogbreed}</h4> <button id="btn">Remove</button></div></div>`
     )};
     divCreator()
+    function clearfields(){
+        const tempCount = count;
+        const button = document.getElementById("btn")
+        button.addEventListener("click", function(event){
+            event.preventDefault();
+            const element = document.getElementbyId(tempCount);
+            element.remove()
+            DOMSelectors.dogbreed.value = "";
+            DOMSelectors.name.value = "";
+            DOMSelectors.pic.value = "";
+        }
+    
+        )};
+        clearfields()
+    
+    
 });
 }
 insert()
 
-DOMSelectors.button.addEventListener("click", function(event){
+
+
+
+
+
+
+
+
+
+
+/* DOMSelectors.button.addEventListener("click", function(event){
     event.preventDefault();
     function clearfields(){
-        const element = document.querySelector("container");
-        element.remove();
+        const container = document.querySelector(".container");
+        container.remove();
         DOMSelectors.dogbreed.value = "";
         DOMSelectors.name.value = "";
         DOMSelectors.pic.value = "";
     }
     clearfields()
 });
-    
+
+function remove(){
+    let btns = document.querySelectorAll(".btn");
+    btns.forEach((button)=> 
+    button.addEventListener('click', function(event){
+        console.log(event.target.parentElement);
+
+    }
+    ));
+}
+remove();
+ */
+
